58fdddea3d4a319ff6ee16045bd2054a662ea039
Michal Nowakowski <michal.nowakowski85@gmail.com>
2024-02-02 16:19:09 +0100

    NumberValidator refactor

--- src/main/java/it/michalnowakowski/domain/numberreceiver/NumberValidator.java
+++ src/main/java/it/michalnowakowski/domain/numberreceiver/NumberValidator.java
@@ -4,10 +4,16 @@ import java.util.Set;
 
 class NumberValidator {
 
+    public static final int NUMBERS_COUNT = 6;
+    public static final int MIN_RANGE = 1;
+    public static final int MAX_RANGE = 99;
+
+
     boolean areAllNumbersInRange(Set<Integer> numbersFromUser) {
         return numbersFromUser.stream()
-                .filter(num -> num >= 1)
-                .filter(num -> num <= 99)
-                .count() == 6;
+                .filter(num -> num >= MIN_RANGE)
+                .filter(num -> num <= MAX_RANGE)
+                .count() == NUMBERS_COUNT;
     }
 }
+

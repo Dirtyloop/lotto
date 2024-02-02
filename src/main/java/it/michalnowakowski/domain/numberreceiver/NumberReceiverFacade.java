55c04fa7bb7b020a3d48b67e85ec9487386e91d4
Michal Nowakowski <michal.nowakowski85@gmail.com>
2024-02-02 14:27:11 +0100

    first tests added - input six numbers

--- src/main/java/it/michalnowakowski/domain/numberreceiver/NumberReceiverFacade.java
+++ src/main/java/it/michalnowakowski/domain/numberreceiver/NumberReceiverFacade.java
@@ -1,4 +1,14 @@
 package it.michalnowakowski.domain.numberreceiver;
 
+import java.util.Set;
+
 public class NumberReceiverFacade {
+
+    public String inpitNumbers(Set<Integer> numbers) {
+        if(numbers.size() != 6) {
+            return "Fail";
+        }
+        return "Succes";
+    }
+
 }
--- src/test/java/it/michalnowakowski/domain/numberreceiver/NumberReceiverFacadeTest.java
+++ src/test/java/it/michalnowakowski/domain/numberreceiver/NumberReceiverFacadeTest.java
@@ -1,12 +1,28 @@
 package it.michalnowakowski.domain.numberreceiver;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import java.util.Set;
 
 class NumberReceiverFacadeTest {
 
     @Test
-    public void test() {
+    public void should_succed_when_six_numbers_in_range_received() {
+        NumberReceiverFacade numberReceiverFacade = new NumberReceiverFacade();
 
+        String result = numberReceiverFacade.inpitNumbers(Set.of(1, 2, 3, 4, 5, 6));
+
+        Assertions.assertThat("Succes").isEqualTo(result);
+    }
+
+    @Test
+    public void should_failed_when_five_numbers_in_range_received() {
+        NumberReceiverFacade numberReceiverFacade = new NumberReceiverFacade();
+
+        String result = numberReceiverFacade.inpitNumbers(Set.of(1, 2, 3, 4, 5));
+
+        Assertions.assertThat("Fail").isEqualTo(result);
     }
 
 }

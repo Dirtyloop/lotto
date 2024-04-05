eef81697e37c4be4e462dd9f92d3ac245c110151
Michal Nowakowski <michal.nowakowski85@gmail.com>
2024-04-05 11:58:34 +0200

    second test passed

--- src/test/java/it/michalnowakowski/domain/numbergenerator/LuckyNumbersGeneratorFacadeTest.java
+++ src/test/java/it/michalnowakowski/domain/numbergenerator/LuckyNumbersGeneratorFacadeTest.java
@@ -1,5 +1,6 @@
 package it.michalnowakowski.domain.numbergenerator;
 
+import java.util.Set;
 import it.michalnowakowski.domain.numbergenerator.dto.LuckyNumbersDto;
 import it.michalnowakowski.domain.numberreceiver.NumberReceiverFacade;
 import org.junit.jupiter.api.Test;
@@ -24,6 +25,21 @@ class LuckyNumbersGeneratorFacadeTest {
         LuckyNumbersDto generatedNumbers = numbersGenerator.generateLuckyNumbers();
 
         assertThat(generatedNumbers.getLuckyNumbers().size()).isEqualTo(6);
+    }
+
+    @Test
+    public void should_return_set_of_numbers_in_required_range() {
+        RandomNumbersGenerable generator = new RandomGenerator();
+        when(numberReceiverFacade.retriveNextDrawDate()).thenReturn(LocalDateTime.now());
+        LuckyNumberGeneratorFacade numbersGenerator = new NumbersGeneratorConfiguration().createForTest(numberReceiverFacade, generator, luckyNumbersRepository);
+
+        LuckyNumbersDto generatedNumbers = numbersGenerator.generateLuckyNumbers();
+
+        int min_range = 1;
+        int max_range = 99;
+        Set<Integer> luckyNmbers = generatedNumbers.getLuckyNumbers();
+        boolean numbersInRange = luckyNmbers.stream().allMatch(number -> number >= min_range && number <= max_range);
+        assertThat(numbersInRange).isTrue();
     }
 
 }
